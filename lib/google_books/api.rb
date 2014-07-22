222cd684978fd581a273250c6c1eba673bed4d57
Vincent Durand <vincent.durand@madwork.org>
2014-07-22 15:56:13 +0200

    Implement API.find method
    Query the Google Books API to find a book by its unique VolumeID

--- lib/google_books/api.rb
+++ lib/google_books/api.rb
@@ -5,8 +5,8 @@ module GoogleBooks
     class << self
 
       # The search parameters.
-      attr_accessor :parameters      
-      
+      attr_accessor :parameters
+
       # Queries the Google Books API. Takes a query string and an
       # optional options hash.
       #
@@ -17,28 +17,37 @@ module GoogleBooks
       # * `:count`, which specifies the number of results per page.
       #
       # * `:api_key`, your [API key](http://code.google.com/apis/books/docs/v1/using.html#auth) for making requests against the Google Books API.
-      def search(query, opts ={})
+      def search(query, opts = {})
         self.parameters = { 'q' => query }
         parameters['startIndex'] = opts[:page]  if opts[:page]
         parameters['maxResults'] = opts[:count] if opts[:count]
         parameters['key'] = opts[:api_key] if opts[:api_key]
-        
-        result = get(url.to_s)
+
         Response.new result
       end
-      
+
+      # Query the Google Books API to find a book by its unique VolumeID.
+      #
+      # The options hash respects the following members:
+      #
+      # * `:api_key`, your [API key](http://code.google.com/apis/books/docs/v1/using.html#auth) for making requests against the Google Books API.
+      def find(id, opts = {})
+        self.parameters = {}
+        parameters['key'] = opts[:api_key] if opts[:api_key]
+
+        Book.new result("/books/v1/volumes/#{id}")
+      end
+
       private
-      
+
       def query
         parameters.
           map { |k, v| "#{k}=#{CGI.escape(v.to_s)}" }.
           join('&')
       end
-      
-      def url
-        URI::HTTPS.build(:host  => 'www.googleapis.com',
-                         :path  => '/books/v1/volumes',
-                         :query => query)
+
+      def result(path = '/books/v1/volumes')
+        get URI::HTTPS.build(host: 'www.googleapis.com', path: path, query: query)
       end
     end
   end
--- spec/google_books/api_spec.rb
+++ spec/google_books/api_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
-  
+
 module GoogleBooks
   describe API do
     use_vcr_cassette 'google'
-    
+
     describe "search" do
       it "should escape spaces" do
         API.search('damien white')
         API.send(:query).should include 'q=damien+white'
       end
-      
+
       it "should set the page" do
         API.search('damien white', :page => 2)
         API.send(:query).should include 'startIndex=2'
@@ -19,29 +19,40 @@ module GoogleBooks
         API.search('damien white', :count => 20)
         API.send(:query).should include 'maxResults=20'
       end
-      
+
       it "should accept in an API key" do
         API.search('damien white', :api_key => 'ABCDEFG')
         API.send(:query).should include 'key=ABCDEFG'
       end
-      
+
       it "should join parameters" do
         API.search('damien white', :count => 20, :page => 2)
         API.send(:query).should include 'startIndex=2'
         API.send(:query).should include 'maxResults=20'
         API.send(:query).should include 'q=damien+white'
-        API.send(:query).count('&').should eq 2 
+        API.send(:query).count('&').should eq 2
       end
-      
+
       it "should return the proper number results based on the count passed in" do
         results = API.search('damien white', :count => 20)
         results.count.should eq 20
       end
-      
+
       it "should return a response" do
         API.search('damien white').should be_a API::Response
       end
-      
+
+    end
+
+    describe "find" do
+      it "should accept in an API key" do
+        API.find('rokQngEACAAJ', :api_key => 'ABCDEFG')
+        API.send(:query).should include 'key=ABCDEFG'
+      end
+
+      it "should return a response" do
+        API.find('rokQngEACAAJ').should be_a API::Book
+      end
     end
   end
 end
